262a7c9459f85c49ce2981071381a595032ef004
Michiel van der Meer <mvdmeer@jewelsoftware.com>
2025-07-14 14:41:41 +0200

    Fixed build warnings

--- src/mvdmio.ASP.Jobs/Internals/Tasks/JobSchedulerSynchronizationContext.cs
+++ src/mvdmio.ASP.Jobs/Internals/Tasks/JobSchedulerSynchronizationContext.cs
@@ -12,12 +12,12 @@ internal sealed class JobSchedulerSynchronizationContext : SynchronizationContex
       _scheduler = scheduler;
    }
 
-   public override void Post(SendOrPostCallback d, object state)
+   public override void Post(SendOrPostCallback d, object? state)
    {
       Task.Factory.StartNew(() => d(state), CancellationToken.None, TaskCreationOptions.None, _scheduler);
    }
 
-   public override void Send(SendOrPostCallback d, object state)
+   public override void Send(SendOrPostCallback d, object? state)
    {
       var t = Task.Factory.StartNew(() => d(state), CancellationToken.None, TaskCreationOptions.None, _scheduler);
       t.Wait();
