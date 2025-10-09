18529f3625ab4ad9a93368240a2b1ce31f19616c
Michiel van der Meer <mvdmeer@jewelsoftware.com>
2025-10-09 12:55:28 +0200

    [no ci] Cleanup

--- src/mvdmio.ASP.Jobs/Internals/Storage/Data/CronJobStoreItem.cs
+++
@@ -1,9 +0,0 @@
-﻿using Cronos;
-
-namespace mvdmio.ASP.Jobs.Internals.Storage.Data;
-
-internal sealed class CronJobStoreItem
-{
-   public required CronExpression CronExpression { get; set; }
-   public required JobStoreItem Job { get; set; }
-}
--- src/mvdmio.ASP.Jobs/Internals/Storage/Data/JobStoreItem.cs
+++ src/mvdmio.ASP.Jobs/Internals/Storage/Data/JobStoreItem.cs
@@ -5,16 +5,20 @@ namespace mvdmio.ASP.Jobs.Internals.Storage.Data;
 
 internal sealed class JobStoreItem
 {
-   public Guid JobId { get; init; } =
-#if NET9_0_OR_GREATER
-      Guid.CreateVersion7();
-#else
-   Guid.NewGuid();
-#endif
-
+   public Guid JobId { get; init; } = NewGuid();
+   
    public required Type JobType { get; init; }
    public required object Parameters { get; init; }
    public required JobScheduleOptions Options { get; init; }
    public required DateTime PerformAt { get; init; }
    public CronExpression? CronExpression { get; init; }
+
+   private static Guid NewGuid()
+   {
+#if NET9_0_OR_GREATER
+      return Guid.CreateVersion7();
+#else
+      return Guid.NewGuid();
+#endif      
+   }
 }
